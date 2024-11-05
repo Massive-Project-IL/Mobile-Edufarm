eac4719636593689dc68378c9e91471a8e69ca66
RendiSinaga <rendysinagaa10@gmail.com>
2024-11-05 13:45:34 +0700

    tambahin ConfirmasiDialog

--- app/src/main/java/com/example/edufarm/HalamanIsiMateri.kt
+++ app/src/main/java/com/example/edufarm/HalamanIsiMateri.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -58,10 +60,10 @@ fun IsiMateriScreen(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(25.dp))
 
-        // Gambar Utama yang memenuhi lebar layar
+        // Gambar Utama yang memenuhi lebar layar, berada di luar area scroll
         Box(modifier = Modifier.fillMaxWidth()) {
             Image(
-                painter = painterResource(id = R.drawable.petani), // Ganti dengan ID gambar yang sesuai
+                painter = painterResource(id = R.drawable.petani),
                 contentDescription = "Gambar Materi",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -72,11 +74,12 @@ fun IsiMateriScreen(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Konten Utama di Kolom dengan Padding
+        // Konten yang dapat di-scroll
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 35.dp, end = 35.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()) // Menambahkan scroll pada kolom konten
+                .padding(horizontal = 35.dp)
         ) {
             // Judul Materi
             Text(
@@ -91,11 +94,21 @@ fun IsiMateriScreen(modifier: Modifier = Modifier) {
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            // Konten Materi
+            // Konten Materi yang panjang
             Text(
-                text = "Menanam kacang tanah yang sehat dimulai dari pemilihan benih berkualitas. Benih yang unggul memiliki ciri berisi penuh, tidak keriput, dan bebas dari penyakit. Proses pemilihan ini penting untuk memastikan hasil panen yang optimal. Tanah yang subur, terhidrasi dengan baik, serta jarak tanam yang tepat akan meningkatkan kemampuan tanaman kacang tanah untuk berkembang secara efisien. Pastikan memilih benih yang sesuai dengan kondisi tanah di wilayah Anda agar hasil panen maksimal.\n" +
-                        "\n" +
-                        "Proses pemilihan benih kacang tanah yang tepat sangat krusial dalam menentukan keberhasilan pertumbuhan tanaman. Benih yang sehat harus dipilih dari tanaman yang tidak terserang hama atau penyakit, serta memiliki ukuran yang seragam dan berwarna cerah. Sebaiknya, hindari penggunaan benih yang terlalu kecil atau rusak, karena hal ini dapat berdampak pada rendahnya produktivitas. Menurut para ahli agronomi, menggunakan benih dengan daya kecambah yang baik bisa meningkatkan hasil panen hingga 20% lebih tinggi dibandingkan benih yang asal-asalan..",
+                text = """
+                    Menanam kacang tanah yang sehat dimulai dari pemilihan benih berkualitas. Benih yang unggul memiliki ciri berisi penuh, tidak keriput, dan bebas dari penyakit. Proses pemilihan ini penting untuk memastikan hasil panen yang optimal. Tanah yang subur, terhidrasi dengan baik, serta jarak tanam yang tepat akan meningkatkan kemampuan tanaman kacang tanah untuk berkembang secara efisien. Pastikan memilih benih yang sesuai dengan kondisi tanah di wilayah Anda agar hasil panen maksimal.
+
+                    Proses pemilihan benih kacang tanah yang tepat sangat krusial dalam menentukan keberhasilan pertumbuhan tanaman. Benih yang sehat harus dipilih dari tanaman yang tidak terserang hama atau penyakit, serta memiliki ukuran yang seragam dan berwarna cerah. Sebaiknya, hindari penggunaan benih yang terlalu kecil atau rusak, karena hal ini dapat berdampak pada rendahnya produktivitas. Menurut para ahli agronomi, menggunakan benih dengan daya kecambah yang baik bisa meningkatkan hasil panen hingga 20% lebih tinggi dibandingkan benih yang asal-asalan.
+
+                    Tanah yang ideal untuk kacang tanah adalah yang gembur dan kaya akan nutrisi. Persiapan tanah yang baik meliputi penggemburan, pemberian pupuk organik, dan pengendalian gulma sebelum tanam. Jika tanah memiliki tekstur yang terlalu keras, akar tanaman akan kesulitan menyerap nutrisi, yang pada akhirnya menghambat pertumbuhan kacang tanah. 
+
+                    Penerapan teknik pemupukan yang tepat akan mendukung pertumbuhan tanaman kacang tanah secara optimal. Disarankan untuk memberikan pupuk kandang atau kompos yang telah matang, karena jenis pupuk ini dapat meningkatkan kualitas tanah secara alami. Selain itu, pengaturan irigasi yang baik sangat penting dalam budidaya kacang tanah, terutama pada fase awal pertumbuhan dimana tanaman membutuhkan kelembaban yang stabil. 
+
+                    Pengendalian hama dan penyakit juga harus dilakukan secara teratur. Beberapa hama utama yang sering menyerang tanaman kacang tanah antara lain kutu daun, ulat grayak, dan lalat kacang tanah. Penggunaan pestisida nabati, seperti ekstrak daun mimba atau bawang putih, dapat membantu mengurangi serangan hama tanpa merusak keseimbangan ekosistem. 
+
+                    Dengan perawatan yang tepat mulai dari pemilihan benih hingga pengendalian hama, petani dapat memastikan bahwa tanaman kacang tanah tumbuh sehat dan menghasilkan panen yang berkualitas tinggi. Proses ini membutuhkan ketelitian dan ketekunan, namun hasilnya akan sepadan dengan usaha yang dikerahkan.
+                """.trimIndent(),
                 fontSize = 10.sp,
                 lineHeight = 20.sp,
                 letterSpacing = (-0.24).sp,
@@ -106,6 +119,8 @@ fun IsiMateriScreen(modifier: Modifier = Modifier) {
         }
     }
 }
+
+
 
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/edufarm/ui/components/ConfirmationDialog.kt
@@ -0,0 +1,141 @@
+package com.example.edufarm.ui.components
+
+// ConfirmationDialog.kt
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+import com.example.edufarm.R
+import com.example.edufarm.ui.theme.poppinsFontFamily
+
+@Composable
+fun ConfirmationDialog(
+    message: String,
+    onDismissRequest: () -> Unit,
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit,
+) {
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        shape = RoundedCornerShape(16.dp),
+        containerColor = colorResource(R.color.background_konfirmasi),
+        properties = DialogProperties(
+            dismissOnClickOutside = false // Mencegah dialog ditutup dengan klik di luar
+        ),
+        text = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                // Gambar di bagian atas
+                Image(
+                    painter = painterResource(id = R.drawable.ic_konfirmasi), // Sesuaikan dengan ID gambar
+                    contentDescription = null,
+                    contentScale = androidx.compose.ui.layout.ContentScale.Crop,
+                    modifier = Modifier
+                        .size(145.dp)
+                        .padding(bottom = 13.dp)
+                )
+
+                // Teks pesan di bawah gambar
+                Text(
+                    text = message,
+                    fontSize = 12.sp,
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    lineHeight = 20.sp,
+                    letterSpacing = (-0.24).sp,
+                    color = colorResource(R.color.black),
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                // Row untuk tombol "Tidak" dan "Ya" di tengah
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    // Tombol "Tidak" dengan border hijau
+                    OutlinedButton(
+                        onClick = onCancel,
+                        shape = RoundedCornerShape(15.dp),
+                        border = BorderStroke(1.dp, colorResource(R.color.green_button)),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(R.color.white)// Warna hijau sesuai desain Anda
+                        ),
+                        contentPadding = PaddingValues(horizontal = 6.dp, vertical = 0.dp),// Mengubah warna border menjadi hijau
+                        modifier = Modifier
+                            .width(77.dp)
+                            .height(32.dp)
+                            .padding(end = 11.5.dp)
+                    ) {
+                        Text(
+                            text = "Tidak",
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Medium,
+                            fontFamily = poppinsFontFamily,
+                            lineHeight = 20.sp,
+                            letterSpacing = (-0.24).sp,
+                            color = colorResource(R.color.black)
+                        )
+                    }
+
+                    // Tombol "Ya"
+                    Button(
+                        onClick = onConfirm,
+                        shape = RoundedCornerShape(15.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.green_button)),
+                        contentPadding = PaddingValues(horizontal = 6.dp, vertical = 0.dp),
+                        modifier = Modifier
+                            .width(77.dp)
+                            .height(32.dp)
+                            .padding(start = 11.5.dp)
+                    ) {
+                        Text(
+                            text = "Ya",
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Medium,
+                            fontFamily = poppinsFontFamily,
+                            lineHeight = 20.sp,
+                            letterSpacing = (-0.24).sp,
+                            color = colorResource(R.color.white)
+                        )
+                    }
+                }
+            }
+        },
+        confirmButton = {},
+        dismissButton = {}
+    )
+}
+
+
+
+
