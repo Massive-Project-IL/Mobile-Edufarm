10e04227526314c035b5bf983248a0918d824571
risi gustiar <risigustiar1234@gmail.com>
2024-11-09 13:25:28 +0700

    buat scroll pada rekomendasi pelatihan

--- app/src/main/java/com/example/edufarm/HalamanBeranda.kt
+++ app/src/main/java/com/example/edufarm/HalamanBeranda.kt
@@ -2,6 +2,7 @@ package com.example.edufarm
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,11 +26,14 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -46,7 +50,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.edufarm.navigation.Routes
 import com.example.edufarm.ui.components.BottomNavigationBar
-import com.example.edufarm.ui.components.CardLive
+import com.example.edufarm.ui.components.ConfirmationDialog
 import com.example.edufarm.ui.theme.EdufarmTheme
 import com.example.edufarm.ui.theme.poppinsFontFamily
 
@@ -88,6 +92,140 @@ fun ContentScreen(
                 }
             }
         }
+    }
+}
+
+@Composable
+fun CardLive() {
+    var showDialog by remember { mutableStateOf(false) }
+    var isNotificationActive by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 37.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 15.dp),
+        colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.card_notif))
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Bertanam Gandum",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(id = R.color.green_title)
+                )
+                Icon(
+                    painter = painterResource(
+                        if (isNotificationActive) R.drawable.notifikasi_aktif else R.drawable.notifikasi_default
+                    ),
+                    contentDescription = "Notifikasi",
+                    tint = colorResource(id = R.color.green_title),
+                    modifier = Modifier
+                        .size(26.dp)
+                        .clickable { isNotificationActive = !isNotificationActive }
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(3.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.live),
+                    contentDescription = null,
+                    modifier = Modifier.size(width = 26.dp, height = 22.dp)
+                )
+                Text(
+                    text = "Sedang Berlangsung",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = poppinsFontFamily,
+                    color = colorResource(id = R.color.green_title)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(0.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(start = 15.dp, top = 0.dp, bottom = 17.dp)
+            ) {
+                Text(
+                    text = "waktu",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = poppinsFontFamily,
+                    color = colorResource(id = R.color.green_title),
+                    modifier = Modifier.padding(bottom = 3.dp)
+                )
+                Text(
+                    text = "09.30–12.30",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = poppinsFontFamily,
+                    color = colorResource(id = R.color.green_title)
+                )
+            }
+            Column(
+                modifier = Modifier.padding(end = 0.dp, top = 0.dp, bottom = 17.dp)
+            ) {
+                Text(
+                    text = "Nama Mentor",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = poppinsFontFamily,
+                    color = colorResource(id = R.color.green_title),
+                    modifier = Modifier.padding(bottom = 3.dp)
+                )
+                Text(
+                    text = "Vodka",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = poppinsFontFamily,
+                    color = colorResource(id = R.color.green_title)
+                )
+            }
+            Button(
+                onClick = { showDialog = true },
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.green)),
+                modifier = Modifier
+                    .padding(start = 10.dp, top = 5.dp)
+                    .width(93.dp)
+                    .height(26.dp),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Text(
+                    text = "Gabung Live",
+                    color = Color.White,
+                    fontSize = 9.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = poppinsFontFamily
+                )
+            }
+        }
+    }
+
+    if (showDialog) {
+        ConfirmationDialog(
+            message = "Apakah Kamu Mau Gabung Live?",
+            onDismissRequest = { showDialog = false },
+            onConfirm = {
+                showDialog = false
+            },
+            onCancel = {
+                showDialog = false
+            }
+        )
     }
 }
 
