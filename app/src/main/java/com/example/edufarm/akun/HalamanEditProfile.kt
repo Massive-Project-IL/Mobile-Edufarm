ef9d1bb2af3b63b51844f668f49fdc865df15c88
zackyzu <zackyzulhida@gmail.com>
2024-12-04 22:21:17 +0700

    Menambahkan PopUp edit foto profile

--- app/src/main/java/com/example/edufarm/akun/HalamanEditProfile.kt
+++ app/src/main/java/com/example/edufarm/akun/HalamanEditProfile.kt
@@ -1,25 +1,37 @@
 package com.example.edufarm.akun
 
+import android.content.Intent
+import android.provider.MediaStore
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -38,6 +50,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,6 +71,8 @@ fun HalamanEditProfile(navController: NavController, modifier: Modifier = Modifi
     var email by remember { mutableStateOf("petanimuda@gmail.com") }
     var phoneNumber by remember { mutableStateOf("") }
     val selectedItem = remember { mutableStateOf("Akun") }
+    var showPopup by remember { mutableStateOf(false) }
+    var showBottomNav by remember { mutableStateOf(true) }
 
     val systemUiController = rememberSystemUiController()
     val topBarColor = colorResource(id = R.color.green)
@@ -71,8 +86,12 @@ fun HalamanEditProfile(navController: NavController, modifier: Modifier = Modifi
 
     Scaffold(
         modifier = modifier,
-        bottomBar = { BottomNavigationBar(navController, selectedItem) }
-    ) { innerPadding ->
+        bottomBar = {
+            if (showBottomNav) { // Tampilkan BottomNavigation hanya jika showBottomNav = true
+                BottomNavigationBar(navController, selectedItem)
+            }
+        }
+    ){ innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -127,13 +146,19 @@ fun HalamanEditProfile(navController: NavController, modifier: Modifier = Modifi
                                 color = colorResource(id = R.color.green_logo),
                                 shape = CircleShape
                             )
-                            .padding(6.dp)
+                            .clickable {
+                                showPopup = true
+                                showBottomNav = false // Sembunyikan Bottom Navigation
+                            }
+
                     ) {
                         Icon(
                             painter = painterResource(id = R.drawable.camera),
                             contentDescription = "Edit Icon",
                             tint = Color.White,
-                            modifier = Modifier.fillMaxSize()
+                            modifier = Modifier
+                                .size(24.dp)
+                                .offset(x = 4.dp, y = 4.dp)
                         )
                     }
                 }
@@ -265,6 +290,8 @@ fun HalamanEditProfile(navController: NavController, modifier: Modifier = Modifi
 
                 Spacer(modifier = Modifier.height(60.dp))
 
+
+
                 // Save Changes Button
                 Button(
                     onClick = { showDialog = true },
@@ -294,6 +321,159 @@ fun HalamanEditProfile(navController: NavController, modifier: Modifier = Modifi
                 },
                 onCancel = { showDialog = false }
             )
+        }
+
+        if (showPopup) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.3f)),
+
+                contentAlignment = Alignment.Center
+            ) {
+                Card(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.2f),
+                    shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+                    elevation = CardDefaults.elevatedCardElevation(8.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color.White)
+                )  {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.close),
+                                contentDescription = "Close Icon",
+                                tint = Color.Black,
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .clickable {
+                                        showPopup = false
+                                        showBottomNav = true
+                                    }
+
+                            )
+                            Spacer(modifier = Modifier.width(120.dp))
+                            Text(
+                                text = "Foto Profil",
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black,
+                                fontFamily = poppinsFontFamily,
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(22.dp))
+
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            // Opsi Kamera
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.clickable {
+                                    // Tambahkan aksi untuk membuka kamera
+                                    showPopup = false
+                                    showBottomNav = true
+                                }
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.camera_2),
+                                    contentDescription = "Kamera",
+                                    modifier = Modifier
+                                        .size(48.dp)
+                                        .border(
+                                            width = 2.dp,
+                                            color = colorResource(id = R.color.gray_icon),
+                                            shape = CircleShape
+                                        )
+                                        .padding(8.dp),
+                                    tint = colorResource(id = R.color.green_logo)
+                                )
+                                Text(
+                                    text = "Kamera",
+                                    fontSize = 12.sp,
+                                    fontFamily = poppinsFontFamily,
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.Black
+                                )
+                            }
+                            // Opsi Galeri
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.clickable {
+                                    // Tambahkan aksi untuk membuka galeri
+                                    showPopup = false
+                                    showBottomNav = true
+                                }
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.photograph),
+                                    contentDescription = "Galeri",
+                                    modifier = Modifier
+                                        .size(48.dp)
+                                        .border(
+                                            width = 2.dp,
+                                            color = colorResource(id = R.color.gray_icon),
+                                            shape = CircleShape
+                                        )
+                                        .padding(8.dp),
+
+                                    tint = colorResource(id = R.color.green_logo)
+                                )
+                                Text(
+                                    text = "Galeri",
+                                    fontSize = 12.sp,
+                                    fontFamily = poppinsFontFamily,
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.Black
+                                )
+                            }
+                            // Opsi Hapus
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.clickable {
+                                    // Tambahkan aksi untuk menghapus foto
+                                    showPopup = false
+                                    showBottomNav = true
+                                }
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.trash),
+                                    contentDescription = "Hapus",
+                                    modifier = Modifier
+                                        .size(48.dp)
+                                        .border(
+                                            width = 2.dp,
+                                            color = colorResource(id = R.color.gray_icon),
+                                            shape = CircleShape
+                                        )
+                                        .padding(8.dp),
+                                    tint = colorResource(id = R.color.green_logo)
+                                )
+                                Text(
+                                    text = "Hapus",
+                                    fontSize = 12.sp,
+                                    fontFamily = poppinsFontFamily,
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.Black
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
