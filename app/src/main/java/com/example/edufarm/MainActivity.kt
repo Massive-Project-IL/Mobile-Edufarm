1a7f88d1d41214dff30486b769a917fbf30c3fc2
RendiSinaga <rendysinagaa10@gmail.com>
2024-12-19 00:05:18 +0700

    Perbaikan

--- app/src/main/java/com/example/edufarm/MainActivity.kt
+++ app/src/main/java/com/example/edufarm/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
 import com.example.edufarm.navigation.EdufarmNavHost
 import com.example.edufarm.ui.theme.EdufarmTheme
@@ -22,13 +19,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             EdufarmTheme {
                 val navController = rememberNavController()
-
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
+                Surface {
                     EdufarmNavHost(
-                        navController = navController,
-                        modifier = Modifier.padding(innerPadding)
+                        navController = navController
                     )
                 }
             }
